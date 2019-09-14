f8da4f4d4bd146a9814b8a8dcd2f23ac66188ca5
Lachezar Balev <lachezar.balev@rewe-digital.com>
2019-09-14 12:38:58 +0300

    Add a backend service

--- minikube.sh
+++ minikube.sh
@@ -7,6 +7,7 @@ if [[ $1 == "delete" ]]; then
   kubectl delete -f minikube-mysql-pvc.yaml
   kubectl delete -f minikube-mysql-pv.yaml
   kubectl delete -f minikube-credentials.yaml
+  kubectl delete -f minikube-backend.yaml
 elif [[ $1 == "create" ]]; then
   kubectl apply -f minikube-config.yaml
   kubectl apply -f minikube-credentials.yaml
@@ -14,6 +15,7 @@ elif [[ $1 == "create" ]]; then
   kubectl apply -f minikube-mysql-pvc.yaml
   kubectl apply -f minikube-mysql-service.yaml
   kubectl apply -f minikube-mysql-deployment.yaml
+  kubectl apply -f minikube-backend.yaml
 else
   echo "Usage: minikube [create|delete]"
 fi
