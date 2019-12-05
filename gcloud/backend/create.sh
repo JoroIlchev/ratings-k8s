b7d795a80f08ab5ebdcfbdca4bbba84409164742
Lachezar Balev (Extern) <lachezar.balev@gmail.com>
2019-12-05 10:37:17 +0200

    Just some minor corrections

--- gcloud/backend/create.sh
+++ gcloud/backend/create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+kubectl apply -f gcloud-backend-config.yaml
 kubectl apply -f gcloud-backend.yaml
 kubectl apply -f gcloud-backend-service.yaml
 kubectl apply -f gcloud-backend-ingress.yaml
--- gcloud/create-mysql.sh
+++ gcloud/create-mysql.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f gcloud-pvc.yaml
 kubectl apply -f gcloud-mysql-credentials.yaml
 kubectl apply -f gcloud-mysql.yaml
 kubectl apply -f gcloud-mysql-service.yaml
