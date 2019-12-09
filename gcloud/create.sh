8450c9ac2c75d4366a472e000d46a0744de5ce1b
Lachezar Balev (Extern) <lachezar.balev@gmail.com>
2019-12-09 11:33:50 +0200

    fix the create script

--- gcloud/create.sh
+++ gcloud/create.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-kubectl apply -f gcloud-pvc.yaml
-kubectl apply -f gcloud-mysql-credentials.yaml
-kubectl apply -f gcloud-mysql.yaml
-kubectl apply -f gcloud-mysql-service.yaml
-kubectl apply -f gcloud-config.yaml
-kubectl apply -f gcloud-backend.yaml
-kubectl apply -f gcloud-backend-service.yaml
-kubectl apply -f gcloud-backend-ingress.yaml
-kubectl apply -f gcloud-frontend.yaml
-kubectl apply -f gcloud-frontend-service.yaml
-kubectl apply -f gcloud-frontend-ingress.yaml
+kubectl apply -f storage/gcloud-pvc.yaml
+kubectl apply -f mysql/gcloud-mysql-credentials.yaml
+kubectl apply -f mysql/gcloud-mysql.yaml
+kubectl apply -f mysql/gcloud-mysql-service.yaml
+kubectl apply -f backend/gcloud-backend-config.yaml
+kubectl apply -f backend/gcloud-backend.yaml
+kubectl apply -f backend/gcloud-backend-service.yaml
+kubectl apply -f backend/gcloud-backend-ingress.yaml
+kubectl apply -f frontend/gcloud-frontend.yaml
+kubectl apply -f frontend/gcloud-frontend-service.yaml
+kubectl apply -f frontend/gcloud-frontend-ingress.yaml
 
